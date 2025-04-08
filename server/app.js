283d6fdacc30c7f5369afc1e0c4c19050c7527cb
saritasubhra <saritasubhra1@gmail.com>
2025-04-08 17:10:00 +0530

    minor changes

--- server/app.js
+++ server/app.js
@@ -4,7 +4,7 @@ const globalErrorHandler = require("./controllers/errorController");
 const feedbackRouter = require("./routes/feedbackRoutes");
 
 const corsOptions = {
-  origin: "http://localhost:5173",
+  origin: process.env.CLIENT_URL,
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   credentials: true,
 };
