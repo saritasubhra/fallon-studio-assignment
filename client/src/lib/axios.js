e2990481e41d58cd1ea67713b98bf1b81fe17beb
saritasubhra <saritasubhra1@gmail.com>
2025-04-08 17:17:59 +0530

    server url

--- client/src/lib/axios.js
+++ client/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:8000/api/v1",
+  baseURL: "https://fallon-studio-assignment.onrender.com/api/v1",
   withCredentials: true, // send cookies to the server
 });
 
